511e60634bc69435b7a4232bbec5486b8187f91f
Simon Lenz <lenz.simon@googlemail.com>
2012-02-25 01:28:06 +0100

    init commit
    Signed-off-by: Simon Lenz <lenz.simon@googlemail.com>

---
+++ src/client/client_main.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import socket
+import time
+import json
+
+SERVERIP = "www.simon-lenz.de"
+SERVERPORT = 52222
+
+if __name__ == "__main__":
+	nick = raw_input("Nickname:")
+
+	while (True):
+		msg = raw_input("Nachricht (\"ende\" zum Beenden):")
+		if msg == "ende":
+			break
+
+		#--nicht unbedingt nötig--
+		#nick_uni = nick.decode(sys.stdin.encoding)
+		#msg_uni = msg.decode(sys.stdin.encoding)
+		#nick_utf8 = nick_uni.encode("utf-8")
+		#msg_utf8 = msg_uni.encode("utf-8")
+		#-------------------------
+
+		data = {"nickname": nick, #nick_utf8,
+				"senderIPPORT": 0,
+				"sendtime": time.time(),
+				"recvtime": 0,
+				"message": msg} #msg_utf8}
+
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.connect((SERVERIP, SERVERPORT))
+		s.send(json.JSONEncoder().encode(data))
+		s.close()
